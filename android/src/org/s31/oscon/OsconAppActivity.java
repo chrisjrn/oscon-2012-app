08a93e183d7455fde0b228cbe12ad7c6091fd9f7
Christopher Neugebauer <chrisjrn@gmail.com>
2012-06-24 16:58:14 +1000

    Adds initial version of the Android app Makes the schedule JSON an asset file.

---
+++ android/src/org/s31/oscon/OsconAppActivity.java
@@ -0,0 +1,13 @@
+package org.s31.oscon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OsconAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
