a6887652d7742c0555ca51614d4f4967331e97c7
Christopher Neugebauer <chrisjrn@gmail.com>
2012-06-27 15:35:35 +1000

    Added viewpager to schedule view

--- android/src/org/s31/oscon/OsconAppActivity.java
+++ android/src/org/s31/oscon/OsconAppActivity.java
@@ -1,5 +1,7 @@
 package org.s31.oscon;
 
+import java.util.ArrayList;
+
 import org.s31.oscon.schedule.ScheduleStructure;
 
 import android.app.ActionBar;
@@ -8,8 +10,11 @@ import android.app.ActionBar.TabListener;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,22 +22,31 @@ import android.view.MenuItem;
 
 public class OsconAppActivity extends Activity {
 	/** Called when the activity is first created. */
+
+	ViewPager mViewPager;
+	TabsAdapter mTabsAdapter;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// setContentView(R.layout.main);
 
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.pager);
+        setContentView(mViewPager);
+		
 		// Notice that setContentView() is not used, because we use the root
 		// android.R.id.content as the container for each fragment
 
 		// setup action bar for tabs
 		ActionBar actionBar = getActionBar();
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-		actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
 		
 		// Turns the app icon into an "up" button.
 		actionBar.setHomeButtonEnabled(true);
 
+        mTabsAdapter = new TabsAdapter(this, mViewPager);
+		
 		// Load tabs for the given day:
 		int day = this.getIntent().getIntExtra("day", 0);
 		int j = 0;
@@ -40,13 +54,14 @@ public class OsconAppActivity extends Activity {
 			Bundle args = new Bundle();
 			args.putInt("day", day);
 			args.putInt("time_slot", j);
-			Tab tab = actionBar
+			/* Tab tab = actionBar
 					.newTab()
 					.setText("" + i)
 					.setTabListener(
 							new SimpleTabListener<ScheduleFragment>(this, ""
-									+ i, ScheduleFragment.class, args));
-			actionBar.addTab(tab);
+									+ i, ScheduleFragment.class, args)); */
+			//actionBar.addTab(tab);
+			mTabsAdapter.addTab(actionBar.newTab().setText("" + i), ScheduleFragment.class, args );
 
 			j++;
 		}
@@ -70,7 +85,8 @@ public class OsconAppActivity extends Activity {
 
 			// This flag unwinds the navigation stack, rather than loading the
 			// new activity in the existing stack.
-			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+					| Intent.FLAG_ACTIVITY_NEW_TASK);
 			startActivity(intent);
 			return true;
 		default:
@@ -130,5 +146,95 @@ class SimpleTabListener<T extends Fragment> implements TabListener {
 
 	public void onTabReselected(Tab tab, FragmentTransaction ft) {
 		// User selected the already selected tab. Usually do nothing.
+	}
+
+}
+
+/**
+ * This is a helper class that implements the management of tabs and all details
+ * of connecting a ViewPager with associated TabHost. It relies on a trick.
+ * Normally a tab host has a simple API for supplying a View or Intent that each
+ * tab will show. This is not sufficient for switching between pages. So instead
+ * we make the content part of the tab host 0dp high (it is not shown) and the
+ * TabsAdapter supplies its own dummy view to show as the tab content. It
+ * listens to changes in tabs, and takes care of switch to the correct paged in
+ * the ViewPager whenever the selected tab changes.
+ */
+class TabsAdapter extends FragmentPagerAdapter implements
+		ActionBar.TabListener, ViewPager.OnPageChangeListener {
+	private final Context mContext;
+	private final ActionBar mActionBar;
+	private final ViewPager mViewPager;
+	private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+
+	static final class TabInfo {
+		private final Class<?> clss;
+		private final Bundle args;
+
+		TabInfo(Class<?> _class, Bundle _args) {
+			clss = _class;
+			args = _args;
+		}
+	}
+
+	public TabsAdapter(Activity activity, ViewPager pager) {
+		super(activity.getFragmentManager());
+		mContext = activity;
+		mActionBar = activity.getActionBar();
+		mViewPager = pager;
+		mViewPager.setAdapter(this);
+		mViewPager.setOnPageChangeListener(this);
+	}
+
+	public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
+		TabInfo info = new TabInfo(clss, args);
+		tab.setTag(info);
+		tab.setTabListener(this);
+		mTabs.add(info);
+		mActionBar.addTab(tab);
+		notifyDataSetChanged();
+	}
+
+	@Override
+	public int getCount() {
+		return mTabs.size();
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		TabInfo info = mTabs.get(position);
+		return Fragment.instantiate(mContext, info.clss.getName(), info.args);
+	}
+
+	@Override
+	public void onPageScrolled(int position, float positionOffset,
+			int positionOffsetPixels) {
+	}
+
+	@Override
+	public void onPageSelected(int position) {
+		mActionBar.setSelectedNavigationItem(position);
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int state) {
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		Object tag = tab.getTag();
+		for (int i = 0; i < mTabs.size(); i++) {
+			if (mTabs.get(i) == tag) {
+				mViewPager.setCurrentItem(i);
+			}
+		}
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
 	}
 }
