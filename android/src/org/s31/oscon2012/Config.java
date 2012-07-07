4db8f322e87be33db7fc92271f3bdbf924ccfbf5
Christopher Neugebauer <chrisjrn@gmail.com>
2012-07-08 08:17:14 +1000

    Initial starting point for the app -- displays nothing at all.

---
+++ android/src/org/s31/oscon2012/Config.java
@@ -0,0 +1,12 @@
+package org.s31.oscon2012;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class Config {
+	
+	public static TimeZone confTimeZone = TimeZone.getTimeZone("America/Los_Angeles"); // The timezone for the conference
+	
+}
