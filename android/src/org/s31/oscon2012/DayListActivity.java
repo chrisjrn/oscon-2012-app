3f1b19166e5d67d4b1be697b56947ce04210e1cd
Christopher Neugebauer <chrisjrn@gmail.com>
2012-07-08 10:46:15 +1000

    Adds the basic day list activity

---
+++ android/src/org/s31/oscon2012/DayListActivity.java
@@ -0,0 +1,23 @@
+package org.s31.oscon2012;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+public class DayListActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+		Fragment f = Fragment
+				.instantiate(this, DayListFragment.class.getName());
+		ft.add(android.R.id.content, f, "default");
+		ft.commit();
+	}
+}
