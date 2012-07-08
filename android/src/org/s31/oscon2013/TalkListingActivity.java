d3a1f031f81f08f77ea76abc6a933843ac2f4a0d
Christopher Neugebauer <chrisjrn@gmail.com>
2012-07-08 17:15:52 +1000

    Adds 'up' navigation to each screen.

--- android/src/org/s31/oscon2013/TalkListingActivity.java
+++ android/src/org/s31/oscon2013/TalkListingActivity.java
@@ -1,9 +1,12 @@
 package org.s31.oscon2013;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class TalkListingActivity extends Activity {
 
@@ -12,6 +15,14 @@ public class TalkListingActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 
+		// Set up the action bar
+		ActionBar actionBar = getActionBar();
+		
+		// Turns the app icon into an "up" button.
+		// A listener is required to make this actually do anything.
+		actionBar.setDisplayHomeAsUpEnabled(true);
+
+		
 		// Fragments are added to the navigations stack using
 		// FragmentTransactions
 		FragmentTransaction ft = getFragmentManager().beginTransaction();
@@ -29,5 +40,25 @@ public class TalkListingActivity extends Activity {
 		ft.commit();
 
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// android.R.id.home is the "up" button.
+			Intent intent = new Intent(this, DayListActivity.class);
+
+			// This flag unwinds the navigation stack, rather than loading the
+			// new activity in the existing stack.
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+					| Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
 
 }
