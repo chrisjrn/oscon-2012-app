d011d0d981e9701d55152058d51bee77fceb3098
Christopher Neugebauer <chrisjrn@gmail.com>
2012-07-08 10:41:45 +1000

    Finished setting up the talk listing fragment.
    Conflicts:
    	.gitignore
    	android/src/org/s31/oscon2013/TalkListingFragment.java

--- android/src/org/s31/oscon2013/TalkListingFragment.java
+++ android/src/org/s31/oscon2013/TalkListingFragment.java
@@ -6,6 +6,9 @@ import java.util.Locale;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class TalkListingFragment extends Fragment {
@@ -23,23 +26,24 @@ public class TalkListingFragment extends Fragment {
 	public TextView mDescriptionView;
 	public TextView mRoomView;
 	public TextView mDateView;
-	
+
 	// Set up some date formatters so we can display the time.
 	public static SimpleDateFormat dateAndTime;
 	public static SimpleDateFormat timeOnly;
 	static {
 		// Tuesday, 17 July 2012 9:00am
-		dateAndTime = new SimpleDateFormat("EEE, d LLLLLL yyyy hh:mma",Locale.US);
+		dateAndTime = new SimpleDateFormat("EEE, d LLLLLL yyyy hh:mma",
+				Locale.US);
 		dateAndTime.setTimeZone(Config.confTimeZone);
 		// 9:00am
-		timeOnly = new SimpleDateFormat("hh:mma",Locale.US);
+		timeOnly = new SimpleDateFormat("hh:mma", Locale.US);
 		timeOnly.setTimeZone(Config.confTimeZone);
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		// Some data that describes a single talk.
 		mTitle = "Level Up Your Apps: Mobile UX Design and Development";
 		mAuthor = "Paris Buttfield Addison, Christopher Neugebauer, Jon Manning";
@@ -55,4 +59,30 @@ public class TalkListingFragment extends Fragment {
 		mUrl = "http://www.oscon.com/oscon2013/public/schedule/detail/29002";
 	}
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.talk_listing_fragment, null);
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		
+		mTitleView = ((TextView)view.findViewById(R.id.talk_listing_title));
+		mTitleView.setText(mTitle);
+
+		mAuthorView = ((TextView)view.findViewById(R.id.talk_listing_author));
+		mAuthorView.setText(mAuthor);
+
+		mRoomView = ((TextView)view.findViewById(R.id.talk_listing_room));
+		mRoomView.setText(mRoom);
+		
+		mDateView = ((TextView)view.findViewById(R.id.talk_listing_date));
+		mDateView.setText(String.format("%s--%s", dateAndTime.format(mStartDate.getTime()),timeOnly.format(mEndDate.getTime())));
+
+		mDescriptionView = ((TextView)view.findViewById(R.id.talk_listing_description));
+		mDescriptionView.setText(mDescription);
+
+	}
+			
 }
