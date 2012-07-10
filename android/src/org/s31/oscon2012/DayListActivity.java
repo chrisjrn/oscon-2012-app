38260bec857edecd8c310b835111d654bdac4c07
Christopher Neugebauer <chrisjrn@gmail.com>
2012-07-11 07:19:50 +1000

    Reorganised the navigation.

--- android/src/org/s31/oscon2012/DayListActivity.java
+++ android/src/org/s31/oscon2012/DayListActivity.java
@@ -4,20 +4,41 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class DayListActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 
 		FragmentTransaction ft = getFragmentManager().beginTransaction();
 		Fragment f = Fragment
 				.instantiate(this, DayListFragment.class.getName());
 		ft.add(android.R.id.content, f, "default");
 		ft.commit();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// android.R.id.home is the "up" button.
+			Intent intent = new Intent(this, ScheduleActivity.class);
+
+			// This flag unwinds the navigation stack, rather than loading the
+			// new activity in the existing stack.
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+					| Intent.FLAG_ACTIVITY_NEW_TASK);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
