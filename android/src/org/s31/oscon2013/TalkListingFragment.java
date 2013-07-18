2c8aac25dd16c79c8ec155033cd16139fa66f8b0
Christopher Neugebauer <chrisjrn@gmail.com>
2013-07-18 10:27:57 -0700

    Moved everything to reflect 2013-ness.

--- android/src/org/s31/oscon2013/TalkListingFragment.java
+++ android/src/org/s31/oscon2013/TalkListingFragment.java
@@ -1,4 +1,4 @@
-package org.s31.oscon2012;
+package org.s31.oscon2013;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -41,18 +41,18 @@ public class TalkListingFragment extends Fragment {
 		super.onCreate(savedInstanceState);
 		
 		// Some data that describes a single talk.
-		mTitle = "Android-Fu: Awesome Apps for Ice Cream Sandwich and Beyond";
+		mTitle = "Level Up Your Apps: Mobile UX Design and Development";
 		mAuthor = "Paris Buttfield Addison, Christopher Neugebauer, Jon Manning";
-		mDescription = "In this session you'll learn why you can't consider UX and design an optional extra when designing mobile apps for Android, how to tell an awesome app from a terrible app, and the basics of both designing and coding for the latest and greatest Android platform (Android 4.0 and beyond). Stylish apps aren't just for that other mobile platform, and Android is surprisingly easy to get started with.";
-		mRoom = "Portland 255";
+		mDescription = "In this session you'll learn why you can't consider UX and design an optional extra when designing mobile apps, and how to tell an awesome app from a terrible app. In this platform-agnostic design-heavy workshop, you'll learn how to build wireframes, how to translate those wireframes into actual working Android code, and how to evaluate your designs for future improvement.";
+		mRoom = "Portland 252";
 
 		mStartDate = Calendar.getInstance(Config.confTimeZone, Locale.US);
-		mStartDate.set(2012, 7, 17, 9, 00);
+		mStartDate.set(2012, 7, 23, 9, 00);
 		
 		mEndDate = Calendar.getInstance(Config.confTimeZone, Locale.US);
-		mEndDate.set(2012, 7, 17, 12, 30);
+		mEndDate.set(2012, 7, 23, 12, 30);
 		
-		mUrl = "http://www.oscon.com/oscon2012/public/schedule/detail/24288";
+		mUrl = "http://www.oscon.com/oscon2013/public/schedule/detail/29002";
 	}
 
 }
